304fec1f14d13adc588b099a4fc660f77451d906
Anton de Groot <adgroot@bkwi.nl>
2026-04-29 15:44:21 +0200

    test: provide TranslateService mock in app test
    Update app.spec.ts to include HttpClientTestingModule and mock TranslateLoader provider so TranslateService is available during testing. Fixes NG0201 provider error in tests.

--- client/src/app/app.spec.ts
+++ client/src/app/app.spec.ts
@@ -1,10 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideTranslateService, TranslateLoader, TranslationObject } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+class MockTranslateLoader implements TranslateLoader {
+  getTranslation(lang: string): Observable<TranslationObject> {
+    return of({});
+  }
+}
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [App, HttpClientTestingModule],
+      providers: [
+        provideTranslateService({
+          loader: {
+            provide: TranslateLoader,
+            useClass: MockTranslateLoader
+          }
+        })
+      ]
     }).compileComponents();
   });
 
